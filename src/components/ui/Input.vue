5aa149f2c34e22593417508415890dee0f1854d0
Lisovsky-UwU <nikish10012@gmail.com>
2024-02-15 15:46:40 +0700

    Input and Button custom component

---
+++ src/components/ui/Input.vue
@@ -0,0 +1,15 @@
+<template>
+  <label for="name" class="block text-gray-700 text-sm font-bold mb-2">{{ props.label }}</label>
+  <input :type="props.type" id="name" name="name" :placeholder="props.placeholder" required class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  label: String,
+  placeholder: String,
+  type: {
+    type: String,
+    default: "text"
+  },
+})
+</script>
