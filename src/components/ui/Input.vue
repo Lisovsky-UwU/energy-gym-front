2f18e1e336667af535c754d67b9c3ef76d41a4af
Lisovsky-UwU <nikish10012@gmail.com>
2024-02-16 01:56:45 +0700

    Registate markup

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -1,6 +1,6 @@
 <template>
-  <label for="name" class="block text-gray-700 text-sm font-bold mb-2">{{ props.label }}</label>
-  <input :type="props.type" id="name" name="name" :placeholder="props.placeholder" required class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+  <label :for="name" class="block text-gray-700 text-sm font-bold mb-2">{{ props.label }}</label>
+  <input :type="props.type" :id="name" :name="name" :placeholder="props.placeholder" required class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500">
 </template>
 
 <script setup lang="ts">
@@ -12,4 +12,6 @@ const props = defineProps({
     default: "text"
   },
 })
+
+const name = Math.random()
 </script>
--- src/views/LoginLk.vue
+++ src/views/LoginLk.vue
@@ -28,8 +28,57 @@
             </div>
 
             <div v-if="openTab === 2" class="transition-all duration-300 bg-white p-4 rounded-lg shadow-md">
-              <h2 class="text-2xl font-semibold mb-2 text-blue-600">Section 2 Content</h2>
-              <p class="text-gray-700">Proin non velit ac purus malesuada venenatis sit amet eget lacus. Morbi quis purus id ipsum ultrices aliquet Morbi quis.</p>
+
+              <div>
+                <div v-if="registrationStep == 1">
+                  <div class="h-auto grid grid-cols-1">
+                    <ui-input label="Студенческий билет" placeholder="12345678"/>
+                  </div>
+                </div>
+
+                <div v-if="registrationStep == 2">
+                  <div class="h-auto gap-2 grid grid-cols-1">
+                    <div>
+                      <ui-input label="Придумайте пароль" placeholder="********"/>
+                    </div>
+                    <div>
+                      <ui-input label="Подтвердите пароль" placeholder="********"/>
+                    </div>
+                  </div>
+                </div>
+
+                <div v-if="registrationStep == 3">
+                  <div class="h-auto gap-2 grid grid-cols-1">
+                    <div>
+                      <ui-input label="Фамилия" placeholder="Иванов"/>
+                    </div>
+                    <div>
+                      <ui-input label="Имя" placeholder="Иван"/>
+                    </div>
+                    <div>
+                      <ui-input label="Отчество" placeholder="Иванович"/>
+                    </div>
+                    <div>
+                      <ui-input label="Группа" placeholder="БИ-12"/>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="mt-5 flex justify-between items-center gap-x-2">
+                  <button type="button" :disabled="registrationStep < 2" class="py-2 px-3 inline-flex items-center gap-x-1 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" @click="registrationStep -= 1">
+                    <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>
+                    Назад
+                  </button>
+                  <button v-if="registrationStep < 3" type="button" class="py-2 px-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" @click="registrationStep += 1">
+                    Далее
+                    <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+                  </button>
+                  <button v-else type="button" class="py-2 px-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" @click="registrate()">
+                    Зарегистрироваться
+                  </button>
+                </div>
+              </div>
+
             </div>
 
           </div>
@@ -44,6 +93,12 @@ import UiInput from '@/components/ui/Input.vue'
 import UiButton from '@/components/ui/Button.vue'
 
 const openTab = ref(1)
+const registrationStep = ref(1)
+
+function registrate() {
+  localStorage.setItem('token', 'user')
+  location.reload()
+}
 
 </script>
 
