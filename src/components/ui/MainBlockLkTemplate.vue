c7fdb5ee1435684dc8c2981e21b1da70f0c828bb
Lisovsky_UwU <nikish10012@gmail.com>
2024-03-29 23:24:47 +0700

    Main block template for LK

---
+++ src/components/ui/MainBlockLkTemplate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-background h-full rounded-t-lg flex flex-col rounded-b-md min-w-64">
+    <div class="bg-second w-full min-h-[68px] text-3xl font-thin rounded-t-md text-white flex place-items-center justify-center">
+      {{ props.title }}
+    </div>
+
+    <div class="overflow-auto">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  title: {
+    type: String
+  }
+})
+</script>
