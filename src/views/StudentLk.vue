613acb18435ecae8e01ec4f2e8ec58a9a1e31568
Lisovsky_UwU <nikish10012@gmail.com>
2024-03-29 23:24:31 +0700

    New colors

--- src/views/StudentLk.vue
+++ src/views/StudentLk.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen overflow-auto bg-slate-600 background-custom flex flex-row p-14 gap-14">
-    <div class="bg-white w-80 rounded-md flex items-center pt-8 flex-col overflow-auto">
+    <div class="bg-background min-w-80 rounded-md flex items-center pt-8 flex-col overflow-auto">
       <router-link to="/lk/profile-settings" class="absolute left-[315px] rounded-3xl bg-second text-white p-2">
         <svg-icon type="mdi" :path="mdiPencil"></svg-icon>
       </router-link>
@@ -11,34 +11,34 @@
         <p class="text-lg">Статус: Студент</p>
       </div>
       <div class="my-4 w-full px-5 gap-3 flex flex-col">
-        <router-link to="/lk" class="menu-btn bg-menu_btn_1 hover:bg-menu_btn_1-hover">
+        <router-link to="/lk" class="menu-btn bg-menu-btn-1 hover:bg-menu-btn-1-hover">
           <svg-icon type="mdi" :path="mdiNewspaper"></svg-icon>
           Главная
         </router-link>
 
-        <router-link to="/lk/create-entry" class="menu-btn bg-menu_btn_2 hover:bg-menu_btn_2-hover">
+        <router-link to="/lk/create-entry" class="menu-btn bg-menu-btn-2 hover:bg-menu-btn-2-hover">
           <svg-icon type="mdi" :path="mdiFountainPen"></svg-icon>
           Записаться
         </router-link>
 
-        <router-link to="/lk/coach-support" class="menu-btn bg-menu_btn_3 hover:bg-menu_btn_3-hover">
+        <router-link to="/lk/coach-support" class="menu-btn bg-menu-btn-3 hover:bg-menu-btn-3-hover">
           <svg-icon type="mdi" :path="mdiPhoneInTalk"></svg-icon>
           Связь с тренером
         </router-link>
 
-        <router-link to="/lk/support" class="menu-btn bg-menu_btn_4 hover:bg-menu_btn_4-hover">
+        <router-link to="/lk/support" class="menu-btn bg-menu-btn-4 hover:bg-menu-btn-4-hover">
           <svg-icon type="mdi" :path="mdiHeadset"></svg-icon>
           Поддержка
         </router-link>
 
-        <button class="menu-btn bg-menu_btn_5 hover:bg-menu_btn_5-hover" @click="logout()">
+        <button class="menu-btn bg-menu-btn-5 hover:bg-menu-btn-5-hover" @click="logout()">
           <svg-icon type="mdi" :path="mdiArrowLeftThick"></svg-icon>
           Выход
         </button>
       </div>
     </div>
     <div class="flex-auto flex flex-col gap-10">
-      <router-link to="/" class="bg-white rounded-md w-fit mx-auto flex items-center gap-5 px-5 py-2">
+      <router-link to="/" class="bg-background rounded-md w-fit mx-auto flex items-center gap-5 px-5 py-2">
         <img src="@/assets/logo_without_back.png" class="h-12">
         <p class="text-[32px] font-light select-none">Энергия СГУГиТ</p>
       </router-link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,29 +7,40 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        background: '#edeff5',
         primary: '#4caf50',
         second: '#1d2e41',
+        accent: '#77b379',
 
-        menu_btn_1: {
-          DEFAULT: '#61b1dd',
-          hover: '#81bbdb'
+        'select-entry': {
+          1: '#8dcf43',
+          2: '#3ea0d6',
+          3: '#eb8119',
+          4: '#7d2880'
         },
-        menu_btn_2: {
-          DEFAULT: '#49ca89',
-          hover: '#67cf9b'
-        },
-        menu_btn_3: {
-          DEFAULT: '#bc5162',
-          hover: '#b5626f'
-        },
-        menu_btn_4: {
-          DEFAULT: '#e17e70',
-          hover: '#de9085'
-        },
-        menu_btn_5: {
-          DEFAULT: '#7d2880',
-          hover: '#7b407d'
-        },
+
+        'menu-btn': {
+          1: {
+            DEFAULT: '#61b1dd',
+            hover: '#81bbdb'
+          },
+          2: {
+            DEFAULT: '#49ca89',
+            hover: '#67cf9b'
+          },
+          3: {
+            DEFAULT: '#bc5162',
+            hover: '#b5626f'
+          },
+          4: {
+            DEFAULT: '#e17e70',
+            hover: '#de9085'
+          },
+          5: {
+            DEFAULT: '#7d2880',
+            hover: '#7b407d'
+          },
+        }
       }
     },
   },
